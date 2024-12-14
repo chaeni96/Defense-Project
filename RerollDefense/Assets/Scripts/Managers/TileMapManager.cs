a8ce61282f44af5909bc8001497708886c98886b
chaeni96 <87288427+chaeni96@users.noreply.github.com>
2024-12-14 13:16:28 +0900

    [ch] 파일 정리

--- RerollDefense/Assets/Scripts/Managers/TileMapManager.cs
+++ RerollDefense/Assets/Scripts/Managers/TileMapManager.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
-public class TileManager : MonoBehaviour
+public class TileMapManager : MonoBehaviour
 {
-    public static TileManager _instance;
+    public static TileMapManager _instance;
 
     public Tilemap tileMap;
     private Dictionary<Vector3Int, TileData> tileDataMap = new Dictionary<Vector3Int, TileData>();
 
-    public static TileManager Instance
+    public static TileMapManager Instance
     {
         get
         {
             if (_instance == null)
             {
-                _instance = FindObjectOfType<TileManager>();
+                _instance = FindObjectOfType<TileMapManager>();
 
                 if (_instance == null)
                 {
-                    GameObject singleton = new GameObject("TileManager");
-                    _instance = singleton.AddComponent<TileManager>();
+                    GameObject singleton = new GameObject("TileMapManager");
+                    _instance = singleton.AddComponent<TileMapManager>();
                     DontDestroyOnLoad(singleton);
                 }
             }
@@ -50,17 +50,17 @@ public class TileManager : MonoBehaviour
     {
         //타일 데이터 초기화, 모든 타일 배치가능 상태로 설정
 
-        foreach(var position in tileMap.cellBounds.allPositionsWithin) //타일맵의 범위 내 모든 셀 좌표
+        foreach (var position in tileMap.cellBounds.allPositionsWithin) //타일맵의 범위 내 모든 셀 좌표
         {
             //해당 좌표에 타일이 실제 존재하면 배치가능 상태로 변경
-            if(tileMap.HasTile(position))
+            if (tileMap.HasTile(position))
             {
                 tileDataMap[position] = new TileData();
             }
 
         }
 
-        ResetTileColors(new Color(1,1,1,0));
+        ResetTileColors(new Color(1, 1, 1, 0));
 
     }
 
@@ -77,7 +77,7 @@ public class TileManager : MonoBehaviour
         TileData tileData = GetTileData(postion);
 
 
-        if(tileData != null && tileData.isAvailable)
+        if (tileData != null && tileData.isAvailable)
         {
             return true;
         }
@@ -90,7 +90,7 @@ public class TileManager : MonoBehaviour
     //특정 타일색 변경
     public void SetTileColor(Vector3Int position, Color color)
     {
-        if(tileMap.HasTile(position))
+        if (tileMap.HasTile(position))
         {
             tileMap.SetTileFlags(position, TileFlags.None);
 
@@ -102,9 +102,9 @@ public class TileManager : MonoBehaviour
     //모든 타일색 초기화
     public void ResetTileColors(Color color)
     {
-        foreach(var position in tileMap.cellBounds.allPositionsWithin)
+        foreach (var position in tileMap.cellBounds.allPositionsWithin)
         {
-            if(tileMap.HasTile(position))
+            if (tileMap.HasTile(position))
             {
                 tileMap.SetTileFlags(position, TileFlags.None);
                 tileMap.SetColor(position, color); //투명으로 바꾸기
@@ -115,7 +115,7 @@ public class TileManager : MonoBehaviour
     //특정 타일 배치 불가능 상태로 변경
     public void SetTileUnavailable(Vector3Int position)
     {
-        if(tileDataMap.ContainsKey(position))
+        if (tileDataMap.ContainsKey(position))
         {
             tileDataMap[position].isAvailable = false;
         }
