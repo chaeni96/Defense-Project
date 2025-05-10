23c1856ee37410fb5a8d911acf47078c64d69eda
김윤하 <galaxykim0611@gmail.com>
2025-05-11 02:30:38 +0900

    [feat] 유물 데이터 불러오기 및 UI 적용

--- Assets/_Project/Scripts/UI/RelicInventoryUI.cs
+++ Assets/_Project/Scripts/UI/RelicInventoryUI.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using BGDatabaseEnum;
+using DefenseProject.Scripts.UI.UIComponents;
+using TMPro;
 using UnityEngine;
 
 [UIInfo("RelicInventoryUI", "RelicInventoryUI", false)]
 
 public class RelicInventoryUI : FloatingPopupBase
 {
+    [SerializeField] private RelicItemComponent relicItemComponent;
+
+    [Header("Item Grid Parent")]
+    [SerializeField] private Transform normalRelicItemParent;
+    [SerializeField] private Transform rareRelicItemParent;
+    [SerializeField] private Transform epicRelicItemParent;
+    [SerializeField] private Transform legendaryRelicItemParent;
+    [SerializeField] private Transform mythicRelicItemParent;
+    
+    [Header("Relic Description")]
+    [SerializeField] private TMP_Text currentSelectedRelicName;
+    [SerializeField] private TMP_Text currentSelectedRelicDescription;
+    
     private FullWindowLobbyDlg lobbyDlg;
 
     public override void InitializeUI()
     {
         base.InitializeUI();
+
+        InitializeRelicItemUI();
     }
 
     public void InitLobbyDlg(FullWindowLobbyDlg lobby)
@@ -23,10 +41,43 @@ public class RelicInventoryUI : FloatingPopupBase
         base.HideUI();
     }
 
-
     public void OnClickCancelBtn()
     {
         UIManager.Instance.CloseUI<RelicInventoryUI>();
         lobbyDlg.SwitchToCampPanel();
+    }
+
+    private void InitializeRelicItemUI()
+    {
+        var list = D_RelicItemData.GetAllRelicItems();
+        
+        foreach (var item in list)
+        {
+            var parent = GetParentByGrade(item);
+            var itemComponent = Instantiate(relicItemComponent, parent);
+            itemComponent.SetData(new RelicItemDataParam(item.f_name, item.f_grade, item.f_description), OnClickRelicItem);
+        }
+        
+        return;
+        
+        // ======================================================================
+        Transform GetParentByGrade(D_RelicItemData data)
+        {
+            return data.f_grade switch
+            {
+                Grade.Normal => normalRelicItemParent,
+                Grade.Rare => rareRelicItemParent,
+                Grade.Epic => epicRelicItemParent,
+                Grade.Legendary => legendaryRelicItemParent,
+                Grade.Mythic => mythicRelicItemParent,
+                _ => null
+            };
+        }
+    }
+
+    private void OnClickRelicItem(RelicItemDataParam param)
+    {
+        currentSelectedRelicName.text = $"º±≈√ : {param.RelicName}";
+        currentSelectedRelicDescription.text = param.RelicDescription;
     }
 }
---
+++ Assets/_Project/Scripts/UI/UIComponents/RelicItemComponent.cs
@@ -0,0 +1,53 @@
+using System;
+using BGDatabaseEnum;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefenseProject.Scripts.UI.UIComponents
+{
+    public class RelicItemComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private Image relicIcon;
+        
+        private string relicName;
+        private Grade relicGrade;
+        private string relicDescription;
+        
+        private Action<RelicItemDataParam> onClickCallback;
+        
+        public void SetData(RelicItemDataParam param, Action<RelicItemDataParam> onClickCallback)
+        {
+            relicName = param.RelicName;
+            relicGrade = param.RelicGrade;
+            relicDescription = param.RelicDescription;
+            this.onClickCallback = onClickCallback;
+            
+            UpdateUI();
+        }
+
+        public void OnClickRelicItemComponent()
+        {
+            onClickCallback?.Invoke(new RelicItemDataParam(relicName, relicGrade, relicDescription));
+        }
+
+        private void UpdateUI()
+        {
+            
+        }
+    }
+
+    public class RelicItemDataParam
+    {
+        public string RelicName;
+        public Grade RelicGrade;
+        public string RelicDescription;
+        
+        public RelicItemDataParam(string relicName, Grade relicGrade, string relicDescription)
+        {
+            RelicName = relicName;
+            RelicGrade = relicGrade;
+            RelicDescription = relicDescription;
+        }
+    }
+}
