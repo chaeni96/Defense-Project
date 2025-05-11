450d92f565696163922c5a0f25752c4499f6c6d9
김윤하 <galaxykim0611@gmail.com>
2025-05-11 23:57:04 +0900

    [feat] RelicItemData 이전, 다음 엔티티 가져오는 메서드 일반화

--- Assets/_Project/Scripts/Datas/RelicItemData.cs
+++ Assets/_Project/Scripts/Datas/RelicItemData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public partial class D_RelicItemData
 {
@@ -10,5 +11,43 @@ public partial class D_RelicItemData
     public static List<D_RelicItemData> GetOwnedRelicItems() 
     {
         return D_RelicItemData.FindEntities(data => data.f_level > 0);
+    }
+
+    public static D_RelicItemData GetNextRelicItemData(D_RelicItemData currentRelicItem)
+    {
+        if (currentRelicItem.Index == GetAllRelicItems().Count - 1)
+        {
+            return GetEntity(0); // 현재 인덱스가 마지막이라면 0번 엔티티 반환
+        }
+        
+        var nextRelicItem = GetEntity(currentRelicItem.Index + 1);
+
+        // 무엇인가 잘못된 경우임.. 발생해서는 안되는..
+        if (nextRelicItem == null)
+        {
+            Debug.LogError("Next relic item is null");
+            return null;
+        }
+
+        return nextRelicItem;
+    }
+    
+    public static D_RelicItemData GetPreviousRelicItemData(D_RelicItemData currentRelicItem)
+    {
+        if (currentRelicItem.Index == 0)
+        {
+            return GetEntity(GetAllRelicItems().Count - 1); // 현재 인덱스가 0이라면 마지막 엔티티 반환
+        }
+        
+        var previousRelicItem = GetEntity(currentRelicItem.Index - 1);
+        
+        // 무엇인가 잘못된 경우임.. 발생해서는 안되는..
+        if (previousRelicItem == null)
+        {
+            Debug.LogError("Previous relic item is null");
+            return null;
+        }
+
+        return previousRelicItem;
     }
 }
